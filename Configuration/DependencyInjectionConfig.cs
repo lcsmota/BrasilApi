9ae2ad29fda0d31258ecf97c0df256d1e21bfc8b
lcsmota <sacul_m@protonmail.com>
2023-08-11 09:08:12 -0300

    feat: add class config for Depend. Injection

---
+++ Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,19 @@
+using BrasilAPI.BrasilApi;
+using BrasilAPI.BrasilApi.Interfaces;
+using BrasilAPI.Services;
+using BrasilAPI.Services.Interfaces;
+
+namespace BrasilAPI.Configuration;
+
+public static class DependencyInjectionConfig
+{
+    public static IServiceCollection AddDependencyInjection(this IServiceCollection services)
+    {
+        services.AddScoped<IAddressService, AddressService>();
+        services.AddScoped<IBankService, BankService>();
+        services.AddScoped<IBrasilApiAddress, BrasilApiAddress>();
+        services.AddScoped<IBrasilApiBank, BrasilApiBank>();
+
+        return services;
+    }
+}
