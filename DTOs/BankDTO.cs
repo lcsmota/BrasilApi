61b5d8e63225f9c15f5c2b8384b22051bb4e606b
lcsmota <sacul_m@protonmail.com>
2023-08-11 08:53:17 -0300

    feat: add bank Dto

---
+++ DTOs/BankDTO.cs
@@ -0,0 +1,9 @@
+namespace BrasilAPI.DTOs;
+
+public class BankDTO
+{
+    public string? Ispb { get; set; }
+    public string? FullName { get; set; }
+    public string? Name { get; set; }
+    public int? Code { get; set; }
+}
