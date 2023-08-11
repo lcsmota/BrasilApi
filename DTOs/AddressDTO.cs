d048a0ac91196402c2ccbfd7cc4096cc54de1680
lcsmota <sacul_m@protonmail.com>
2023-08-11 08:52:52 -0300

    feat: add address Dto

---
+++ DTOs/AddressDTO.cs
@@ -0,0 +1,10 @@
+namespace BrasilAPI.DTOs;
+
+public class AddressDTO
+{
+    public string? Street { get; set; }
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public string? Neighborhood { get; set; }
+    public string? Cep { get; set; }
+}
